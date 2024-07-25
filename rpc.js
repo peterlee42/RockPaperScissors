291cebe9d7bd90d9a2bb2cf7f4c6bd54017d4613
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2024-07-24 23:43:44 -0400

    Added function that loops rounds n times from a given parameter

--- rpc.js
+++ rpc.js
@@ -20,27 +20,39 @@ function getHumanChoice() {
     return userChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
     if (humanChoice === computerChoice) {
         return "TIE!";
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
         return "Humanity is restored! Human wins!";
     } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
         return "AI defeated humanity... Robots win...";
     } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
         return "Humanity is restored! Human wins!";
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
         return "AI defeated humanity... Robots win...";
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
         return "Humanity is restored! Human wins!";
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
         return "AI defeated humanity... Robots win...";
     } else {
         return "Invalid input!";
     }
 }
 
-let humanChoice = getComputerChoice();
-let computerChoice = getHumanChoice();
-console.log(`Human chose ${humanChoice} and Computer choice ${computerChoice}`)
-console.log(playRound(humanChoice, computerChoice));
+function playGame(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(`Human chose ${humanChoice} and Computer choice ${computerChoice}`)
+        console.log(playRound(humanChoice, computerChoice, humanScore, computerScore));
+    }
+}
+
+playGame(5);
